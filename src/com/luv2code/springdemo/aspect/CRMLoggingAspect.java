1df7b40473bb5f9c8ff23871c38a6fc20ea41e73
yakubalkis <alkis.yakub@gmail.com>
2023-03-20 00:29:17 +0300

    Added Logger using Logger api.

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -1,7 +1,22 @@
 package com.luv2code.springdemo.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class CRMLoggingAspect {
 	
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	
+	// setup pointcut declarations
+	
+	// add @Before advice
+	
+	// add @AfterReturning advice
 	
 	
 }
